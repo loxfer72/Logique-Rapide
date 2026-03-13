de9e8087d65a14fc011f4ecc55faf18f0b5d15b5
Dylan <dylan.gorget@hotmail.fr>
2026-03-13 11:03:20 +0100

    feat: fonction de salutation avec majuscule au prénom

--- String.py
+++ String.py
@@ -1,4 +1,13 @@
-def character_count(string):
-    print(len(string.replace(' ', '')))
+import re
 
-character_count("Bonjour le monde !")
+def character_count(string: str) -> str:
+    return len(string.replace(' ', ''))
+
+print("fonction 1 :")
+print(character_count("Bonjour le monde !"),'\n')
+
+def salutation(name: str) -> str:
+    return "Bonjour " + re.sub(r"\w+", lambda m: m.group().capitalize(), name)
+
+print("fonction 2 :")
+print(salutation("jean-pierre"),'\n')

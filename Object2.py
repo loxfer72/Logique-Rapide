b643b3d824ff81cd90547a5feebf9fe133567f44
Dylan <dylan.gorget@hotmail.fr>
2026-03-13 14:21:03 +0100

    feat: fonction fusionnant deux objets

--- Object2.py
+++ Object2.py
@@ -18,3 +18,12 @@ def transform_values(d: dict, func) -> dict:
 prices_in_euros = {"book": 20, "pen": 5, "notebook": 10}
 to_dollars = lambda euros: euros * 1.1
 print(transform_values(prices_in_euros, to_dollars)) # {"book": 22.0, "pen": 5.5, "notebook": 11.0}
+
+
+#fonction fusion deux objets
+def merge_objects(d1: dict, d2: dict) -> dict:
+    return {k: d1.get(k, 0) + d2.get(k, 0) for k in list(d1) + [k for k in d2 if k not in d1]}
+
+store1_sales = {"january": 1000, "february": 1200, "march": 900}
+store2_sales = {"january": 800, "february": 950, "march": 1100}
+print(merge_objects(store1_sales, store2_sales)) # {"january": 1800, "february": 2150, "march": 2000}
